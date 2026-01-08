c5250d3691ef9ca1a06a5fee7bddc7c5bb80a991
Alexander Tiu <alextiu0525@gmail.com>
2026-01-08 07:06:48 -0600

    Safety commit

---
+++ gen_chem_subpage/flash_cards.js
@@ -0,0 +1,21 @@
+// For now, we will have variables that have very simple information regarding
+// molecular compounds. Later we will move to a more complex structure.
+
+const flashCardsFront = [
+    "B2H6", 
+    "CH4", 
+    "SiH4", 
+    "NH3"
+];
+const flashCardsBack = [
+  "Diborane",
+  "Methane",
+  "Silane",
+  "Ammonia",
+];
+
+let resetFlashCards;
+let showAnswer;
+let nextFlashCard;
+let previousFlashCard;
+
